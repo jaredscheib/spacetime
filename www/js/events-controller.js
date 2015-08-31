b3475530abcf9ef3dbe2e11520786c31b0b5b3ce
jaredscheib <jared.scheib@gmail.com>
2015-08-30 20:31:57 -0700

    Add angular and events controller, populate list with event stub data titles

---
+++ www/js/events-controller.js
@@ -0,0 +1,17 @@
+angular.module('spacetime')
+
+.controller('eventsCtrl', ['$scope', '$http', function($scope, $http){
+  $scope.init = function(){
+    $scope.events = [];
+    $scope.retrieveEvents();
+  };
+
+  $scope.retrieveEvents = function(){
+    $http.get('/events')
+    .then(function(res){
+      $scope.events = res.data;
+    }, function(res){
+      console.log('Error on GET request when trying to retrieve events');
+    });
+  };
+}]);
