61a3d13097d3b67348dcb72c3122cbce8f4d1e09
jaredscheib <jared.scheib@gmail.com>
2015-08-30 17:42:11 -0700

    Set up basic events route and error handling for express server and its config

--- server/server-config.js
+++ server/server-config.js
@@ -5,3 +5,21 @@ var methodOverride = require('method-override');
 var cors = require('cors');
 // (bring in SQL dependency?)
 
+var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride());
+app.use(express.static(__dirname + '/../www'));
+app.use(cors());
+
+app.use('/events', require('./events'));
+
+app.use('*', function(req, res, next){
+  var err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+module.exports = app;
