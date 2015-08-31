0799e1e8f43bcb64d1ce13e27a0dbdd36a997c5d
jaredscheib <jared.scheib@gmail.com>
2015-08-30 17:06:40 -0700

    Add dependencies to server config

---
+++ server/server-config.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cors = require('cors');
+// (bring in SQL dependency?)
+
