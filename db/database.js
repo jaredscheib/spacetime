207f4b7eb81bcca89d24c305764e19edbab85ca2
jaredscheib <jared.scheib@gmail.com>
2015-09-28 20:11:26 -0700

    Correct typo

--- db/database.js
+++ db/database.js
@@ -3,12 +3,13 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
+  port: 3307,
   database: 'spacetimedb'
 });
 
 connection.connect(function(err) {
   if (err) {
-    console.error('Error connceting to MySQL database: ' + err.stack);
+    console.error('Error connecting to MySQL database: ' + err.stack);
     return;
   } else {
     console.log('Connected to database as id ' + connection.threadId);
