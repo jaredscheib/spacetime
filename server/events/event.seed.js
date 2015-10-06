7194756e091d78cf8b748569685823065fc1f72d
jaredscheib <jared.scheib@gmail.com>
2015-10-05 20:01:05 -0700

    Add more seed data

--- server/events/event.seed.js
+++ server/events/event.seed.js
@@ -9,10 +9,26 @@ exports.event_data = [
 },
 {
   title: 'Movie Night',
-  date: 'Sept 10, 2015',
+  date: 'Sept 22, 2015',
   time: '4:15AM',
   location: 'UN Civic Center',
   description: 'Come watch a classic with #strangerFriends!',
   host_name: 'Mike'
+},
+{
+  title: 'Least Fave Book Salon',
+  date: 'Oct 10, 2015',
+  time: '11:30AM',
+  location: '16th St Mission BART',
+  description: 'Read the worst among the foot traffic.',
+  host_name: 'Heather'
+},
+{
+  title: 'Wild Things Coronation',
+  date: 'Oct 15, 2015',
+  time: '10:45PM',
+  location: '16th St Mission BART',
+  description: 'Dress up for a rumpus!',
+  host_name: 'Heather'
 }
 ];
