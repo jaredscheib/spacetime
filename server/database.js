13aa83ff58f85a0bdb22d72e03b90ad7969fc7f2
jaredscheib <jared.scheib@gmail.com>
2015-10-07 18:18:53 -0700

    Add environmental variables for db credentials since live on Azure

--- server/database.js
+++ server/database.js
@@ -1,9 +1,14 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'spacetimedb'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  options: {
+    encrypt: true
+  }
 });
 
 connection.connect(function(err) {
